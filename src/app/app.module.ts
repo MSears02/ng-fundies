5ffe1f46e238094a21fbde1c9907e81b29027f40
msears02@gmail.com <Matt@Matthews-iMac.local>
2019-11-15 19:53:02 -0500

    completed part 5 of training; updated Node and NPM and packages to latest versions inorder to solve DI failure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService} from './events/shared/event.service'
+import {ToastrService } from './common/toastr.service'
 
 @NgModule({
   imports: [
@@ -16,6 +19,11 @@ import { NavBarComponent } from './nav/navbar.component';
     EventThumbnailComponent,
     NavBarComponent
   ], 
-  bootstrap: [EventsAppComponent]
+  providers: [
+    EventService,
+    ToastrService  
+  ],
+  bootstrap: [EventsAppComponent],
+  
 })
 export class AppModule { }
