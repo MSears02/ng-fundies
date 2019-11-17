e418cc78b254a3a4da05ca81e8f4ad2d422a0102
msears02@gmail.com <Matt@Matthews-iMac.local>
2019-11-16 20:22:17 -0500

    Completed Part 6 of Training

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {RouterModule} from '@angular/router'
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index'
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService} from './events/shared/event.service'
-import {ToastrService } from './common/toastr.service'
-import {EventDetailsComponent} from './events/event-details/event-details.component'
-import {appRoutes} from './routes'
-import {CreateEventComponent} from './events/create-event.component'
-import {Error404Component} from './errors/404.component'
-import {EventRouteActivator} from './events/event-details/event-route-activator.service'
+import { ToastrService } from './common/toastr.service'
+import { appRoutes} from './routes'
+import { CreateEventComponent} from './events/create-event.component'
+import { Error404Component} from './errors/404.component'
+
 
 
 @NgModule({
@@ -33,6 +37,7 @@ import {EventRouteActivator} from './events/event-details/event-route-activator.
     EventService,
     ToastrService,
     EventRouteActivator,
+    EventListResolver,
     {provide:'canDeactivateCreateEvent', useValue: checkDirtyState}  
   ],
   bootstrap: [EventsAppComponent],
